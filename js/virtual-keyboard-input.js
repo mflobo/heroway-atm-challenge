afad2f5fda986f4ce06a546a2fdfe715520e6edc
Matheus Lobo <matheus.f.lobo@gmail.com>
2019-08-14 01:34:52 -0300

    Add virtual keyboard functionality to the number pad

---
+++ js/virtual-keyboard-input.js
@@ -0,0 +1,3 @@
+clearAll = () => document.getElementById("display").value = "";
+
+virtualInput = (number) => document.getElementById("display").value = document.getElementById("display").value + number;
